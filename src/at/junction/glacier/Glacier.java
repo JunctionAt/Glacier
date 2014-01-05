ddaf10797b7dfa364c6157211c69c6fad1079e99
Tom Powell <zifnab@zifnab06.net>
2014-01-05 13:00:30 -0700

    Update ArrayList to Map<WorldName, HashSet<BlockHash>>

--- src/at/junction/glacier/Glacier.java
+++ src/at/junction/glacier/Glacier.java
@@ -7,12 +7,12 @@ import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 import javax.persistence.PersistenceException;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -24,7 +24,8 @@ public class Glacier extends JavaPlugin {
     GlacierListener listener = new GlacierListener(this);
     Configuration config = new Configuration(this);
     LiquidTable lt = new LiquidTable(this);
-    List<Location> frozenBlocks = new ArrayList<>();
+    //List<Location> frozenBlocks = new ArrayList<>();
+    Map<String, HashSet<Long>> frozenBlocks = new HashMap<>();
     List<String> frozenPlayers = new ArrayList<>();
     WorldGuardPlugin wg;
     
@@ -92,12 +93,12 @@ public class Glacier extends JavaPlugin {
     }
     
     public void newFrozen(Block block) {
-        frozenBlocks.add(block.getLocation());
+        frozenBlocks.get(block.getWorld().getName()).add(hashLocation(block.getLocation()));
         lt.newFrozen(block.getWorld().getName(), block.getX(), block.getY(), block.getZ());
     }
     
     public void delFrozen(Location loc) {
-        frozenBlocks.remove(loc);
+        frozenBlocks.get(loc.getWorld().getName()).remove(hashLocation(loc));
         lt.delFrozen(loc.getWorld().getName(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
     }
     
@@ -125,5 +126,15 @@ public class Glacier extends JavaPlugin {
         }
 
         return fIDs.equals(tIDs);
+    }
+
+    public Long hashLocation(int x, int y, int z){
+        long hash_X = (long)(x + 32000000) & 0x0fffffff;
+        long hash_Y = (long)y & 0xff;
+        long hash_Z = (long)(x + 32000000) & 0x0fffffff;
+        return (hash_X << 28) | (hash_Y << 58) | hash_Z;
+    }
+    public Long hashLocation(Location l){
+        return hashLocation(l.getBlockX(), l.getBlockY(), l.getBlockZ());
     }
 }
