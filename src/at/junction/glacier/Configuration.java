ff01fbf908235ae9ad843dfa4883c632e542100b
Tom Powell <zifnab@zifnab06.net>
2014-04-07 18:12:17 -0600

    Implements change that should allow water to flow anywhere. Water will no longer break rails, powered rails, activator rails, detector rails,  redstone wire, redstone comparators, or redstone repeaters

--- src/at/junction/glacier/Configuration.java
+++ src/at/junction/glacier/Configuration.java
@@ -1,20 +1,34 @@
 package at.junction.glacier;
 
+import org.bukkit.Material;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Configuration {
     Glacier plugin;
 
     public boolean FREEZE_LAVA;
     public boolean ALLOW_TOGGLE;
     public boolean DEBUG;
+    public List<Material> BLOCK_WATER_DESTROY_LIST;
 
     public Configuration(Glacier plugin) {
         this.plugin = plugin;
     }
 
     public void load() {
+        BLOCK_WATER_DESTROY_LIST = new ArrayList<>();
         FREEZE_LAVA = plugin.getConfig().getBoolean("freeze-lava", true);
         ALLOW_TOGGLE = plugin.getConfig().getBoolean("allow-toggle", true);
         DEBUG = plugin.getConfig().getBoolean("debug", false);
+        for (String mat : plugin.getConfig().getStringList("material-list")) {
+            try {
+                BLOCK_WATER_DESTROY_LIST.add(Material.getMaterial(mat));
+            } catch (Exception e) {
+                plugin.getLogger().warning(String.format("Block type %s not found", mat));
+            }
+        }
     }
 
     public void save() {
--- src/at/junction/glacier/GlacierListener.java
+++ src/at/junction/glacier/GlacierListener.java
@@ -8,7 +8,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.*;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
-import org.bukkit.material.Dispenser;
+import org.bukkit.event.block.BlockFromToEvent;
+
 
 class GlacierListener implements Listener {
 
@@ -22,7 +23,7 @@ class GlacierListener implements Listener {
     public void onBlockPlace(BlockPlaceEvent event) {
         //Only moderators should have water/lava blocks. THIS IS NOT BUCKETS, ONLY WATER/LAVA BLOCK ITEMS.
         if (event.getBlock().isLiquid()) {
-            if (!(event.getPlayer().hasPermission("glacier.flowing") && !plugin.frozenPlayers.contains(event.getPlayer().getName()))) {
+            if (plugin.frozenPlayers.contains(event.getPlayer().getName())) {
                 plugin.newFrozen(event.getBlock());
             }
         }
@@ -39,8 +40,7 @@ class GlacierListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockFromTo(BlockFromToEvent event) {
-        if (plugin.frozenBlocks.get(event.getBlock().getWorld().getName()).contains(plugin.hashLocation(event.getBlock().getLocation()))) {
-            //If FROM is frozen, cancel event
+        if (plugin.config.BLOCK_WATER_DESTROY_LIST.contains(event.getToBlock().getType())){
             event.setCancelled(true);
         } else if (!plugin.canFlowInRegion(event.getBlock(), event.getToBlock())) {
             //If all regions in FROM do NOT match all regions in TO, cancel event
@@ -49,7 +49,6 @@ class GlacierListener implements Listener {
             //If TO is in frozenBlocks, remove TO from frozenBlocks and allow it to flow.
             plugin.delFrozen(event.getToBlock().getLocation());
         }
-
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
@@ -66,27 +65,14 @@ class GlacierListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockFade(BlockFadeEvent event) {
+        //I *think* this stops ice melting, not 100% sure
         if (event.getBlock().getType() == Material.ICE) {
             event.setCancelled(true);
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onBlockDispense(BlockDispenseEvent event) {
-        Material mat = event.getItem().getType();
-        if (mat == Material.LAVA_BUCKET || mat == Material.WATER_BUCKET) {
-            Dispenser dispenser = (Dispenser) event.getBlock().getState().getData();
-            plugin.newFrozen(event.getBlock().getRelative(dispenser.getFacing()));
-        }
-    }
-
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerBucketFill(PlayerBucketFillEvent event) {
-        if (plugin.config.DEBUG) {
-            plugin.getLogger().info(String.format("Filled bucket at %s", event.getBlockClicked().getRelative(event.getBlockFace()).getLocation()));
-            plugin.getLogger().info(String.format("Hash %s", plugin.hashLocation(event.getBlockClicked().getRelative(event.getBlockFace()).getLocation())));
-
-        }
         Material mat = event.getItemStack().getType();
         if (mat == Material.LAVA_BUCKET || mat == Material.WATER_BUCKET) {
             plugin.delFrozen(event.getBlockClicked().getRelative(event.getBlockFace()).getLocation());
@@ -95,16 +81,8 @@ class GlacierListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
-        if (plugin.config.DEBUG) {
-            plugin.getLogger().info(String.format("Empty bucket at %s", event.getBlockClicked().getRelative(event.getBlockFace())));
-            plugin.getLogger().info(String.format("Hash %s", plugin.hashLocation(event.getBlockClicked().getRelative(event.getBlockFace()).getLocation())));
-        }
-
         if (plugin.frozenPlayers.contains(event.getPlayer().getName())) {
             //If player is frozen, place frozen block
-            plugin.newFrozen(event.getBlockClicked().getRelative(event.getBlockFace()));
-        } else if (!event.getPlayer().hasPermission("glacier.flowing") && (!plugin.canPlaceFlowingLiquid(event.getBlockClicked().getRelative(event.getBlockFace()), event.getPlayer().getName()))) {
-            //If player does not have permission AND cannot place a block, freeze it
             plugin.newFrozen(event.getBlockClicked().getRelative(event.getBlockFace()));
         } else if (plugin.config.FREEZE_LAVA && event.getBucket() == Material.LAVA_BUCKET) {
             //If we always freeze lava, freeze it regardless of previous statements
